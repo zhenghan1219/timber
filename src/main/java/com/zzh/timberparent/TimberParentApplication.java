f9c9da7929a10b6547f1e7f8013d99c805052505
zhenghan1219 <zhenghan1219@163.com>
2023-04-08 18:25:53 +0800

    木材交易系统

---
+++ src/main/java/com/zzh/timberparent/TimberParentApplication.java
@@ -0,0 +1,13 @@
+package com.zzh.timberparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimberParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimberParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzh/timberparent/TimberParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzh.timberparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimberParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
